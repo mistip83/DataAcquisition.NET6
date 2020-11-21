9162ecba24bf7d653ea8863748d84248e6a02c2f
mistip83 <65766909+mistip83@users.noreply.github.com>
2020-11-21 09:57:54 +0000

    Added DeviceSimulator project

---
+++ Istip.DataAcquisition.DeviceSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Istip.DataAcquisition.DeviceSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
