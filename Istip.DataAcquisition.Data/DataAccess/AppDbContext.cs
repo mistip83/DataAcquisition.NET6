79e7c0f9316ac9a936b20729ed9fd5ab77cbc3bd
mistip83 <65766909+mistip83@users.noreply.github.com>
2020-11-29 12:52:36 +0000

    Added DbContext

---
+++ Istip.DataAcquisition.Data/DataAccess/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Istip.DataAcquisition.Core.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Istip.DataAcquisition.Data.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppConfiguration> AppConfigurations { get; set; }
+        public DbSet<ApplicationInfo> ApplicationInfo { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Experiment> Experiments { get; set; }
+        public DbSet<Facility> Facilitys { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Workstation> Workstations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
