14e20172ca62f7e32ab851090e50d052573410a1
mistip83 <65766909+mistip83@users.noreply.github.com>
2020-12-25 09:39:09 +0000

    Removed unnecessary usings

--- Istip.DataAcquisition.Data/DataAccess/AppDbContext.cs
+++ Istip.DataAcquisition.Data/DataAccess/AppDbContext.cs
@@ -1,8 +1,5 @@
 ﻿using Istip.DataAcquisition.Core.Models.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Istip.DataAcquisition.Data.DataAccess
 {
