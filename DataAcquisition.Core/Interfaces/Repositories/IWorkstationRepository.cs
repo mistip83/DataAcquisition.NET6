241a872da710931e85bdd9cb2f5d1e5fce2db51f
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-02-07 21:25:04 +0000

    Added WorkstationRepository

--- DataAcquisition.Core/Interfaces/Repositories/IWorkstationRepository.cs
+++ DataAcquisition.Core/Interfaces/Repositories/IWorkstationRepository.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using DataAcquisition.Core.Models.Entities;
 
 namespace DataAcquisition.Core.Interfaces.Repositories
 {
     public interface IWorkstationRepository : IRepository<Workstation>
     {
-        Task<Workstation> GetWorkstationWithExperimentsByWorkstationIdAsync(int id);
+        Task<Workstation> GetWorkstationWithExperimentsByWorkstationIdAsync(Guid id);
     }
 }
---
+++ DataAcquisition.Data/Repositories/WorkstationRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using DataAcquisition.Core.Interfaces.Repositories;
+using DataAcquisition.Core.Models.Entities;
+using DataAcquisition.Data.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAcquisition.Data.Repositories
+{
+    public class WorkstationRepository : Repository<Workstation>, IWorkstationRepository
+    {
+        private AppDbContext AppDbContext => Context as AppDbContext;
+
+        public WorkstationRepository(DbContext context) : base(context)
+        {
+        }
+
+        public async Task<Workstation> GetWorkstationWithExperimentsByWorkstationIdAsync(Guid id)
+        {
+            return await AppDbContext.Workstations.Include(x => x.Experiments)
+                .SingleOrDefaultAsync(x => x.WorkStationId == id);
+        }
+    }
+}
