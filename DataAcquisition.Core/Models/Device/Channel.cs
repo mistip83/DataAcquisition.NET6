069c6c9e668681511daa0d087e72accb1a55c8d3
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-02-13 16:59:47 +0000

    Added Device models

---
+++ DataAcquisition.Core/Models/Device/Channel.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAcquisition.Core.Models.Device
+{
+    public class Channel
+    {
+        public int ChannelNo { get; set; }
+        public string ChannelName { get; set; }
+        public string ChannelAddress { get; set; }
+        public string ChannelUnit { get; set; }
+    }
+}
---
+++ DataAcquisition.Core/Models/Device/Configuration.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace DataAcquisition.Core.Models.Device
+{
+    public class Configuration
+    {
+        public string DeviceId { get; set; }
+        public IEnumerable<Channel> ChannelList { get; set; }
+    }
+}
