1170e778a78bf42a10cb7ce2093a5307352c91f2
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-02-15 21:38:25 +0000

    Update DbContext

--- DataAcquisition.Data/DataAccess/AppDbContext.cs
+++ DataAcquisition.Data/DataAccess/AppDbContext.cs
@@ -14,15 +14,17 @@ namespace DataAcquisition.Data.DataAccess
         public DbSet<ApplicationInfo> ApplicationInfo { get; set; }
         public DbSet<Experiment> Experiments { get; set; }
         public DbSet<Facility> Facilities { get; set; }
-        public DbSet<Company> Companies { get; set; }
+        public DbSet<Company> Company { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Workstation> Workstations { get; set; }
+        public DbSet<Device> Devices { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Apply configurations
             modelBuilder.ApplyConfiguration(new ApplicationInfoConfiguration());
             modelBuilder.ApplyConfiguration(new ExperimentConfiguration());
+            modelBuilder.ApplyConfiguration(new DeviceConfiguration());
             modelBuilder.ApplyConfiguration(new FacilityConfiguration());
             modelBuilder.ApplyConfiguration(new CompanyConfiguration());
             modelBuilder.ApplyConfiguration(new UserConfiguration());
