9b7d1de0fb87b90b66063142bea4e3bf0c2fc15e
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-02-20 08:49:08 +0000

    Updated UserRepository

--- DataAcquisition.Core/Interfaces/Repositories/IUserRepository.cs
+++ DataAcquisition.Core/Interfaces/Repositories/IUserRepository.cs
@@ -6,6 +6,18 @@ namespace DataAcquisition.Core.Interfaces.Repositories
 {
     public interface IUserRepository : IRepository<User>
     {
+        /// <summary>
+        /// Returns a user entity with its all experiments
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         Task<User> GetUserWithExperimentsAsync(string email);
+
+        /// <summary>
+        /// Returns a user entity with its company
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        Task<User> GetUserWithCompanyAsync(string email);
     }
 }
--- DataAcquisition.Data/Repositories/UserRepository.cs
+++ DataAcquisition.Data/Repositories/UserRepository.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace DataAcquisition.Data.Repositories
 {
+    /// <summary>
+    /// Encapsulate the logic required to access data sources
+    /// </summary>
     public class UserRepository : Repository<User>, IUserRepository
     {
         private AppDbContext AppDbContext => Context as AppDbContext;
@@ -14,9 +17,25 @@ namespace DataAcquisition.Data.Repositories
         {
         }
 
+        /// <summary>
+        /// Implementation detail
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
         public async Task<User> GetUserWithExperimentsAsync(string email)
         {
             return await AppDbContext.Users.Include(x => x.Experiments)
+                .SingleOrDefaultAsync(x => x.Email == email);
+        }
+
+        /// <summary>
+        /// Implementation detail
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public async Task<User> GetUserWithCompanyAsync(string email)
+        {
+            return await AppDbContext.Users.Include(x => x.Company)
                 .SingleOrDefaultAsync(x => x.Email == email);
         }
     }
