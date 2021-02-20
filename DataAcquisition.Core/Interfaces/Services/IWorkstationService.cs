1af9b69f6e6ce43da6a1881fd5f32e33d78b62b0
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-02-20 09:48:21 +0000

    Added Workstation service

---
+++ DataAcquisition.Core/Interfaces/Services/IWorkstationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using DataAcquisition.Core.Models.Entities;
+
+namespace DataAcquisition.Core.Interfaces.Services
+{
+    public interface IWorkstationService
+    {
+        /// <summary>
+        /// Returns workstation entity with its experiments
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task<Workstation> GetWorkstationWithExperimentsAsync(Guid id);
+    }
+}
---
+++ DataAcquisition.Service/Services/WorkstationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using DataAcquisition.Core.Interfaces.Repositories;
+using DataAcquisition.Core.Interfaces.Services;
+using DataAcquisition.Core.Interfaces.UnitOfWorks;
+using DataAcquisition.Core.Models.Entities;
+
+namespace DataAcquisition.Service.Services
+{
+    /// <summary>
+    /// Communicate with the API
+    /// </summary>
+    public class WorkstationService : Service<Workstation>, IWorkstationService
+    {
+        public WorkstationService(IUnitOfWork unitOfWork, IRepository<Workstation> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public async Task<Workstation> GetWorkstationWithExperimentsAsync(Guid id)
+        {
+            return await UnitOfWork.Workstations.GetWorkstationWithExperimentsAsync(id);
+        }
+    }
+}
