fbd138b6ea8d477511672282738214f209e27e41
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-03-07 13:53:38 +0000

    Updated license info

--- DataAcquisition.Model/License/LicenseInfo.cs
+++ DataAcquisition.Model/License/LicenseInfo.cs
@@ -1,7 +1,16 @@
-﻿namespace DataAcquisition.Model.License
+﻿using System;
+
+namespace DataAcquisition.Model.License
 {
+    /// <summary>
+    /// Model for license info comes from authorize.lic file
+    /// </summary>
     public class LicenseInfo
     {
-        public string SecretKey { get; set; }
+        public string UserEmail { get; }
+        public string MachineId { get; }
+        public DateTime LicenseBeginDate { get; }
+        public DateTime LicenseEndDate { get; }
+        public DateTime OsDateTime { get; set; }
     }
 }
