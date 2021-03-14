113bea7a74f1c0d24b1c121bfbe4f5cfd2cfbef1
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-03-14 12:05:09 +0000

    Added company and user seed data

---
+++ DataAcquisition.DataAccessEF/SeedData/CompanySeed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataAcquisition.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAcquisition.DataAccessEF.SeedData
+{
+    public class CompanySeed : IEntityTypeConfiguration<Company>
+    {
+        private readonly Company _company;
+
+        public CompanySeed(Company company)
+        {
+            _company = company;
+        }
+
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasData(_company);
+        }
+    }
+}
