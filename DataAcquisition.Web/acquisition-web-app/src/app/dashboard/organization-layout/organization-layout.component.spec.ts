979c2ed02b036cb8acc1b5c8e76036aab1529988
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-05-22 10:42:20 +0100

    Added organization-layout.component

---
+++ DataAcquisition.Web/acquisition-web-app/src/app/dashboard/organization-layout/organization-layout.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { OrganizationLayoutComponent } from './organization-layout.component';
+
+describe('OrganizationLayoutComponent', () => {
+  let component: OrganizationLayoutComponent;
+  let fixture: ComponentFixture<OrganizationLayoutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OrganizationLayoutComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrganizationLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
