4283f13c392881b487152cbbc7a6ed424f1cfcf8
murat istipliler <mistip83@gmail.com>
2021-07-10 07:13:29 +0300

    Added DeviceManager project

---
+++ DataAcquisition.DeviceManager/DeviceManager.cs
@@ -0,0 +1,19 @@
+﻿using DataAcquisition.Interface.DeviceManager;
+using System;
+
+namespace DataAcquisition.DeviceManager
+{
+    public class DeviceManager : IDeviceManager
+    {
+        private const int ReadBufferSize = 8;
+        private readonly Random _random = new();
+
+        public byte[] ReadData(int channelAddress)
+        {
+            var bytes = new byte[ReadBufferSize];
+            _random.NextBytes(bytes);
+
+            return bytes;
+        }
+    }
+}
