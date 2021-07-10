6fb2c152849819409c5930a393422fc28cf98615
murat istipliler <mistip83@gmail.com>
2021-07-10 07:12:25 +0300

    Added ConnectionManager project

---
+++ DataAcquisition.ConnectionManager/ConnectionManager.cs
@@ -0,0 +1,24 @@
+﻿using System.IO.Ports;
+using DataAcquisition.Interface.ConnectionManager;
+
+namespace DataAcquisition.ConnectionManager
+{
+    public class ConnectionManager : IConnectionManager
+    {
+        private SerialPort _serialPort;
+        public void ConnectToDevice()
+        {
+            _serialPort = new SerialPort("COM1", 19200, Parity.None, 8, StopBits.One)
+            {
+                Handshake = Handshake.None, WriteTimeout = 500
+            };
+
+            _serialPort.Open();
+        }
+
+        public void ConnectToSimulator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
