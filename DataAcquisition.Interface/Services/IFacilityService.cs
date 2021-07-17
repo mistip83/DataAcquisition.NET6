adfcdd4b34ffa3a91e5748cf1013a6b9104162e5
murat istipliler <mistip83@gmail.com>
2021-07-17 03:01:05 +0300

    Updated Facility Service

--- DataAcquisition.Interface/Services/IFacilityService.cs
+++ DataAcquisition.Interface/Services/IFacilityService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using DataAcquisition.Model.Entities;
 
@@ -12,5 +13,25 @@ namespace DataAcquisition.Interface.Services
         /// <param name="id"></param>
         /// <returns></returns>
         public Task<Facility> GetFacilityWithWorkStationsAsync(Guid id);
+
+        /// <summary>
+        /// Returns facility entity
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task<Facility> GetFacilityInfoAsync(Guid id);
+
+        /// <summary>
+        /// Returns facility list
+        /// </summary>
+        /// <returns></returns>
+        public Task<IEnumerable<Facility>> GetFacilityList();
+
+        /// <summary>
+        /// Edit facility
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Facility EditFacility(Facility facility);
     }
 }
