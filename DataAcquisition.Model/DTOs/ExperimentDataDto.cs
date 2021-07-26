74a869fdeec0881f58e83841cdb9dbd0c9816bcc
murat istipliler <mistip83@gmail.com>
2021-07-26 20:16:29 +0300

    Added ExperimentDataDto

---
+++ DataAcquisition.Model/DTOs/ExperimentDataDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DataAcquisition.Model.DTOs
+{
+    public class ExperimentDataDto
+    {
+        public IEnumerable<string> ChannelNames { get; set; }
+        public IEnumerable<string> ChannelUnits { get; set; }
+        public string Data { get; set; }
+    }
+}
