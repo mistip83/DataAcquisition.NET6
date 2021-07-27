bb1510abefc0ab56ea8c677d1f2f7d41a1e65577
murat istipliler <mistip83@gmail.com>
2021-07-27 08:34:05 +0300

    Added experiment service

---
+++ DataAcquisition.Service/ExperimentService.cs
@@ -0,0 +1,20 @@
+﻿using DataAcquisition.Interface.Services;
+using DataAcquisition.Model.Entities;
+using System.Threading.Tasks;
+using DataAcquisition.Interface.Repositories;
+using DataAcquisition.Interface.UnitOfWorks;
+
+namespace DataAcquisition.Service
+{
+    public class ExperimentService : Service<Experiment>, IExperimentService
+    {
+        public ExperimentService(IUnitOfWork unitOfWork, IRepository<Experiment> repository) : base(unitOfWork, repository)
+        {
+        }
+
+        public async Task<ExperimentData> GetExperimentDataAsync(int id)
+        {
+            return await UnitOfWork.Experiments.GetExperimentDataAsync(id);
+        }
+    }
+}
