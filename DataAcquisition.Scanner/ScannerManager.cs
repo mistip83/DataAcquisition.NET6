ce7e5ed1b24baeb9b84e1f4ae5b5a297264fed7e
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-08-17 18:46:26 +0100

    Created ScannerManager

---
+++ DataAcquisition.Scanner/ScannerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DataAcquisition.Interface.ScannerManager;
+
+namespace DataAcquisition.Scanner
+{
+    public class ScannerManager : IScannerManager
+    {
+        private const int ReadBufferSize = 8;
+        private readonly Random _random = new Random();
+
+        public double[] ReadData(int[] channelAddressList)
+        {
+            var data = new double[channelAddressList.Length - 1];
+            for (var i = 0; i < channelAddressList.Length; i++)
+            {
+                data[i] = ReadData(channelAddressList[i]);
+            }
+
+            return data;
+        }
+
+        private double ReadData(int channelAddress)
+        {
+            var bytes = new byte[ReadBufferSize];
+            _random.NextBytes(bytes);
+
+            return ConvertRawData(bytes);
+        }
+
+        private static double ConvertRawData(byte[] rawData)
+        {
+            return BitConverter.ToDouble(rawData, 0);
+        }
+    }
+}
