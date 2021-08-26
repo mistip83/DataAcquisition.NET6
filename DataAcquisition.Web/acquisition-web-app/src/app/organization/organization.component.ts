b987344abcd6f7d674e83c14cb9452052fe10202
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-08-26 23:22:32 +0100

    Added Organization service

--- DataAcquisition.Web/acquisition-web-app/src/app/organization/organization.component.ts
+++ DataAcquisition.Web/acquisition-web-app/src/app/organization/organization.component.ts
@@ -1,15 +1,20 @@
+import { Organization } from './../models/organization.model';
 import { Component, OnInit } from '@angular/core';
+import { OrganizationService } from '../services/organization.service';
 
 @Component({
   selector: 'app-organization',
   templateUrl: './organization.component.html',
-  styleUrls: ['./organization.component.css']
+  styleUrls: ['./organization.component.css'],
 })
 export class OrganizationComponent implements OnInit {
+  organization: Organization;
 
-  constructor() { }
+  constructor(private _organizationService: OrganizationService) {}
 
   ngOnInit(): void {
+    this._organizationService
+      .getOrganizationLayout()
+      .subscribe((data) => (this.organization = data));
   }
-
 }
