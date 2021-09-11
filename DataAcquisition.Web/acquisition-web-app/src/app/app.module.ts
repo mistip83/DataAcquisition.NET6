012d60ab8053b1fd9173c807937a5508c3edc580
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-09-11 08:49:34 +0100

    Added sidenav manu and css for content area

--- DataAcquisition.Web/acquisition-web-app/src/app/app.module.ts
+++ DataAcquisition.Web/acquisition-web-app/src/app/app.module.ts
@@ -4,21 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { OrganizationComponent } from './organization/organization.component';
-import { WorkstationComponent } from './workstation/workstation.component';
-import { DeviceComponent } from './device/device.component';
-import { AboutComponent } from './about/about.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { WorkstationComponent } from './workstation/workstation.component';
+import { DeviceComponent } from './device/device.component';
+import { AboutComponent } from './about/about.component';
 import { FacilityComponent } from './facility/facility.component';
 import { FacilityFormComponent } from './facility/facility-form/facility-form.component';
 import { ExperimentComponent } from './experiment/experiment.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -44,6 +47,8 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     MatProgressSpinnerModule,
     MatExpansionModule,
+    MatSidenavModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent],
