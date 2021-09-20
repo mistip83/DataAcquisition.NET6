deed98719cb5c2eafdad38849f7ca873bbb25629
Murat Istipliler <65766909+mistip83@users.noreply.github.com>
2021-09-20 16:57:53 +0100

    Added CalibrationController

---
+++ DataAcquisition.API/Controllers/CalibrationController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using DataAcquisition.Core.Interfaces.Services;
+using DataAcquisition.Core.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace DataAcquisition.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalibrationController : ControllerBase
+    {
+        private readonly ICalibrationService _calibrationService;
+        private readonly IDeviceService _deviceService;
+        private readonly IMapper _mapper;
+
+        public CalibrationController(ICalibrationService calibrationService, 
+            IDeviceService deviceService, IMapper mapper)
+        {
+            _calibrationService = calibrationService ?? throw new ArgumentNullException(nameof(calibrationService));
+            _deviceService = deviceService ?? throw new ArgumentNullException(nameof(deviceService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        /// <summary>
+        /// Calibrate Device
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> CalibrateDevice(int id)
+        {
+            var device = await _deviceService.GetByIdAsync(id);
+            device = _calibrationService.CalibrateDevice(device);
+
+            return Ok(_mapper.Map<DeviceDto>(device));
+        }
+    }
+}
