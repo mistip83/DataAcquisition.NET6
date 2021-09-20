662dd0911f36300649344d9aa9a0fb9c8f7a1891
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-09-20 22:58:08 +0100

    Added comments

--- DataAcquisition.CalibrationManager/CalibrationDecorator.cs
+++ DataAcquisition.CalibrationManager/CalibrationDecorator.cs
@@ -12,8 +12,16 @@ namespace DataAcquisition.CalibrationManager
             _calibrator = calibrator;
         }
 
+        /// <summary>
+        /// After calibration process completes, it returns last calibration date
+        /// </summary>
+        /// <param name="calibrationData"></param>
         public DateTime ApplyCalibrationData(double[] calibrationData) => DateTime.UtcNow;
 
+        /// <summary>
+        /// Returns calibration data from scanner
+        /// </summary>
+        /// <param name="channelAddressList"></param>
         public virtual double[] GetCalibrationData(int[] channelAddressList)
         {
             return _calibrator.GetCalibrationData(channelAddressList);
--- DataAcquisition.Core/Interfaces/CalibrationManager/ICalibration.cs
+++ DataAcquisition.Core/Interfaces/CalibrationManager/ICalibration.cs
@@ -4,7 +4,16 @@ namespace DataAcquisition.Core.Interfaces.CalibrationManager
 {
     public interface ICalibration
     {
-        double[] GetCalibrationData(int[] channelAddressList);
+        /// <summary>
+        /// After calibration process completes, it returns last calibration date
+        /// </summary>
+        /// <param name="calibrationData"></param>
         public DateTime ApplyCalibrationData(double[] calibrationData);
+
+        /// <summary>
+        /// Returns calibration data from scanner
+        /// </summary>
+        /// <param name="channelAddressList"></param>
+        double[] GetCalibrationData(int[] channelAddressList);
     }
 }
