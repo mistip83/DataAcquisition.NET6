b2e8b9843c9c06f2d58754eefee840bd3c8cdbce
Murat Istipliler <65766909+mistip83@users.noreply.github.com>
2021-09-20 17:05:35 +0100

    Removed CalibrateDevice from DeviceService andd added to CalibrationService

--- DataAcquisition.Service/DeviceService.cs
+++ DataAcquisition.Service/DeviceService.cs
@@ -1,5 +1,4 @@
-﻿using DataAcquisition.Core.Interfaces.DeviceManager;
-using DataAcquisition.Core.Interfaces.Repositories;
+﻿using DataAcquisition.Core.Interfaces.Repositories;
 using DataAcquisition.Core.Interfaces.Services;
 using DataAcquisition.Core.Interfaces.UnitOfWorks;
 using DataAcquisition.Core.Models.Entities;
@@ -11,16 +10,8 @@ namespace DataAcquisition.Service
     /// </summary>
     public class DeviceService : Service<Device>, IDeviceService
     {
-        private readonly IDeviceManager _deviceManager;
-        public DeviceService(IUnitOfWork unitOfWork, IRepository<Device> repository, 
-            IDeviceManager deviceManager) : base(unitOfWork, repository)
+        public DeviceService(IUnitOfWork unitOfWork, IRepository<Device> repository) : base(unitOfWork, repository)
         {
-            _deviceManager = deviceManager;
-        }
-
-        public Device CalibrateDevice(Device device)
-        {
-            return _deviceManager.CalibrateDevice(device);
         }
     }
 }
