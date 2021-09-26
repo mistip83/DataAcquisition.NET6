fb8e6c94eff91c4ddec75de804bff9cd60e047eb
mistip83 <65766909+mistip83@users.noreply.github.com>
2021-09-26 10:31:14 +0100

    Added http header to serverConfig

--- DataAcquisition.Web/acquisition-web-app/src/app/services/facility.service.ts
+++ DataAcquisition.Web/acquisition-web-app/src/app/services/facility.service.ts
@@ -1,24 +1,15 @@
 import { ServerConfig } from './serverConfig';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { FacilityDto } from '../models/facilityDto';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class FacilityService {
   constructor(
     private httpClient: HttpClient,
     private serverConfig: ServerConfig
   ) {}
-
-  // Http Options
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-    }),
-  };
 
   getFacilityList(): Observable<FacilityDto[]> {
     return this.httpClient.get<FacilityDto[]>(
@@ -30,7 +21,7 @@ export class FacilityService {
     return this.httpClient.post<FacilityDto>(
       this.serverConfig.REST_API_SERVER_URL + 'facility/add-facility',
       JSON.stringify(facility),
-      this.httpOptions
+      this.serverConfig.httpOptions
     );
   }
 }
