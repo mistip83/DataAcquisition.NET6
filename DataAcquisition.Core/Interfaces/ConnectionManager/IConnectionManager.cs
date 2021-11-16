e175da374be4e556492239042af83934f19ba9fe
Murat Istipliler <65766909+mistip83@users.noreply.github.com>
2021-11-16 10:41:58 +0000

    Added Connection type switch case

--- DataAcquisition.Core/Interfaces/ConnectionManager/IConnectionManager.cs
+++ DataAcquisition.Core/Interfaces/ConnectionManager/IConnectionManager.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
-using DataAcquisition.Core.Enums;
+﻿using DataAcquisition.Core.Enums;
 
 namespace DataAcquisition.Core.Interfaces.ConnectionManager
 {
     public interface IConnectionManager
     {
         void Connect(ConnectionType connectionType);
-        void Connect(List<ConnectionType> connectionTypeList);
         void Disconnect(ConnectionType connectionType);
     }
 }
