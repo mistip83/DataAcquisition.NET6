9267c970fa6cee62d1762313a49c40c693eb2ce8
Murat Istipliler <65766909+mistip83@users.noreply.github.com>
2022-03-12 07:10:32 +0000

    Delete DataAcquisition.Web/acquisition-web-app directory

--- DataAcquisition.Web/acquisition-web-app/src/app/interfaces/organization.ts
+++
@@ -1,6 +0,0 @@
-import { IFacilityName } from "./facility-name";
-
-export interface IOrganization {
-    companyName: string;
-    facilities: IFacilityName[];
-}
--- DataAcquisition.Web/acquisition-web-app/src/app/interfaces/workstation-name.ts
+++
@@ -1,8 +0,0 @@
-import { IDeviceName } from './device-name';
-import { IExperimentName } from "./experiment-name";
-
-export interface IWorkstationName {
-    workstationName: string;
-    devices: IDeviceName[];
-    experiments: IExperimentName[];
-}
