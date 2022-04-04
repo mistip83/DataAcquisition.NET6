12afe38d4f632098cddd4ac8fe4b935412be114c
Murat Istipliler <65766909+mistip83@users.noreply.github.com>
2022-04-04 14:07:54 +0100

    Removed timer

--- DataAcquisition.CalibrationManager/Decorator/TemperatureCalibration.cs
+++ DataAcquisition.CalibrationManager/Decorator/TemperatureCalibration.cs
@@ -1,11 +1,9 @@
-﻿using System.Timers;
-using DataAcquisition.Core.Interfaces.CalibrationManager;
+﻿using DataAcquisition.Core.Interfaces.CalibrationManager;
 
 namespace DataAcquisition.CalibrationManager.Decorator;
 
 public class TemperatureCalibration : CalibrationDecorator
 {
-    private static Timer _timer;
     public TemperatureCalibration(ICalibration calibration) : base(calibration)
     {
     }
@@ -18,18 +16,6 @@ public class TemperatureCalibration : CalibrationDecorator
 
     private static void SetTimer()
     {
-        // Create a timer with a two second interval.
-        _timer = new Timer(2000)
-        { 
-            // _timer.Elapsed += OnTimedEvent;
-            AutoReset = true,
-            Enabled = true
-        };
-    }
-
-    private static void OnTimedEvent(object source, ElapsedEventArgs e)
-    {
-        Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
-            e.SignalTime);
+        // Create a timer
     }
 }
